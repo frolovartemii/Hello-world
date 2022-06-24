03779d1c47c0960264a4939198c6f3949f3823b1
frolovartemii <107404896+frolovartemii@users.noreply.github.com>
2022-06-24 11:01:58 +0300

    Add files via upload

---
+++ Hello_world_bow.py
@@ -0,0 +1,50 @@
+import Enemies
+import time
+ogr = Enemies.Enemy(8)
+Hero = Enemies.Player(10)
+def bow_fight (hp_i, hp_e, health_list, damage_bow_i_simple, damage_bow_i_significant, damage_bow_i_max, damage_enemy):
+    import random
+    while hp_i > 0 and ogr.hp_e > 0:
+        coin = random.randint (1,24)
+        coin_1 = random.randint(1,24)
+        print(f"Вам выпало {coin}.")
+        print(f"Врагу выпало {coin_1}.")
+        if coin == 1:
+            Hero.be_attacked(damage_bow_i_simple, "Вы попали в себя.")
+            time.sleep(2)
+        elif coin > 1 and coin < 12:
+            print(" Вы промазали.")
+            time.sleep(2)
+        elif coin > 11 and coin < 17:
+            ogr.be_attacked(damage_bow_i_simple, 'урон')
+            time.sleep(2)
+        elif coin > 16 and coin < 24:
+            ogr.be_attacked(damage_bow_i_significant, 'значительный урон')
+            time.sleep(2)
+        elif coin == 24:
+            print(" Неужели? Вам выпал максимум? Тогда вы попали и нанесли максимальный урон своим оружием.")
+            ogr.be_attacked(damage_bow_i_max, "максимальный урон")
+            time.sleep(2)
+        if "кровотечение" in health_list:
+            Hero.being_poisoned()
+        if len(health_list) > 5:
+            Hero.max_len_helth_list()
+        if coin_1 == 1:
+            ogr.be_attacked(damage_bow_i_simple,"Враг попал в самого себя.")
+            time.sleep(2)
+        elif coin_1 > 1 and coin_1 < 12:
+            print(" Враг промазал.")
+            time.sleep(2)
+        elif coin_1 > 15:
+            Hero.be_attacked(damage_enemy, "Враг попал в вас.")
+    if hp_i > hp_e:
+        print(" Вы убили врага.")
+        if hp_i <= 0:
+            print(" Но, увы, вы тоже погибли!")
+    elif hp_e > hp_i:
+        print(" Враг убил вас.")
+        if hp_e <= 0:
+            print(" Но вы успели смертельно ранить противника, и он тоже умер.")
+    return hp_i, hp_e, health_list
+
+Hero.hp_i, ogr.hp_e, Hero.health_list = bow_fight(Hero.hp_i, ogr.hp_e, Hero.health_list, Hero.damage_bow_i_simple, Hero.damage_bow_i_significant, Hero.damage_bow_i_max, ogr.damage_enemy)
